054d6c64fd1ecaf282a6b15e71fa1e20625a9c3a
unknown <@311-05.corp.college.ru>
2026-09-12 15:30:35 +0900

    lol

---
+++ loltask2.cs
@@ -0,0 +1,6 @@
+ //Console.Write("Ввод рубля в доллар: ");
+ //string input = Console.ReadLine();
+ //int number = Convert.ToInt32(input);
+ //int onedollar = 84;
+ //int result = int.Parse(input) / onedollar;
+ //Console.WriteLine(result);
